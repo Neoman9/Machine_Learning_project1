b9ad209ff3d03c5e84376499d96f147a25e5de68
Neoman9 <nelsonbenedict35@yahoo.com>
2023-08-04 13:07:46 +0100

    model trainer completed

--- housing/component/model_trainer.py
+++ housing/component/model_trainer.py
@@ -2,9 +2,11 @@ from housing.exception import HousingException
 from housing.logger import logging
 from housing.entity.config_entity import ModelTrainerConfig
 from housing.entity.artifact_entity import ModelTrainerArtifact,DataIngestionArtifact,DataTransformationArtifact
-from housing.util.util import load_numpy_array_data,load_data
-
+from housing.util.util import load_numpy_array_data,load_object, save_object
+from housing.entity.model_factory import MetricInfoArtifact,ModelFactory, GridSearchBestModel, evaluate_regression_model
 import sys,os
+
+from typing import List
 
 
 class  HousingEstimatorModel:
@@ -56,7 +58,50 @@ class ModelTrainer:
             logging.info(f" Extracting model config file path")
 
             model_config_file_path = self.model_trainer_config.model_config_file_path
+
+            logging.info (f" initializing model factory class using above model config file: {model_config_file_path} ")
+            model_factory = ModelFactory(model_config_path=model_config_file_path)
+
+            base_accuracy = self.model_trainer_config.base_accuracy
+            logging.info(f"Expected accuracy: {base_accuracy}")
+
+            logging.info(f" initiating operation model selection")
+            best_model = model_factory.get_best_model(X=x_train,y=y_train, base_accuracy=base_accuracy)
+
+            logging.info(f" best model found on training dataset: {best_model}")
+
+            logging.info(f" extracting trained model list")
+            grid_searched_best_model_list: List[GridSearchBestModel]=model_factory.grid_searched_best_model_list
+
+            model_list = [model.best_model for model in grid_searched_best_model_list]
+            logging.info(f" evaluating all trained model on training and testing dataset both")
+            metric_info: MetricInfoArtifact = evaluate_regression_model(model_list=model_list,X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test,base_accuracy=base_accuracy)
+
+            logging.info(f"Best model found on both training and testing dataset")
+
+            preprocessing_obj = load_object(file_path=self.data_transformation_artifact.preprocessed_object_file_path)
+            model_object = metric_info.model_object
+
+            trained_model_file_path = self.model_trainer_config.trained_model_file_path
+            housing_model= HousingEstimatorModel(preprocessing_object=preprocessing_obj,trained_model_object=model_object)
+            logging.info(f"Saving model at path: {trained_model_file_path}")
+            save_object(file_path=trained_model_file_path,obj=housing_model)
+
+            model_trainer_artifact = ModelTrainerArtifact(is_trained=True,
+                                                          message="model trained successfuly",
+                                                          trained_model_file_path=trained_model_file_path,
+                                                          train_rmse=metric_info.train_rmse,
+                                                          test_rmse=metric_info.test_rmse,
+                                                          train_accuracy=metric_info.train_accuracy,
+                                                          test_accuracy=metric_info.test_accuracy,
+                                                          model_accuracy=metric_info.model_accuracy)
             
+            logging.info(f"Model Trainer Artifact : {model_trainer_artifact}")
+            return model_trainer_artifact
+
+
+
+
 
         except Exception as e:
             raise HousingException(sys,e ) from e
--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,6 +1,7 @@
 from housing.component.data_ingestion import DataIngestion
 from housing.component.data_validation import DataValidation
 from housing.component.data_transformation import DataTransformation
+from housing.component.model_trainer import ModelTrainer
 from housing.exception import HousingException
 from housing.logger import logging
 import sys,os
@@ -10,7 +11,7 @@ from threading import Thread
 from housing.entity.experiment import Experiment
 from housing.config.configuration import configuration 
 from housing.constants import EXPERIMENT_DIR_NAME,EXPERIMENT_FILE_NAME
-from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 
 Experiment = namedtuple("Experiment",["experiment_id", "initialization_timestamp","artifact_time_stamp","running_status",
                                       "start_time","stop_time","execution_time","message","experiment_file_path","accuracy","is_model_accepted"])
@@ -48,6 +49,7 @@ class Pipeline(Thread):
         except Exception as e:
             raise HousingException(sys,e) from e 
         
+    
     def  start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact)-> DataTransformationArtifact:
         try:
             data_transformation = DataTransformation(data_transformation_config=self.config.get_data_transformation_config(),data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
@@ -55,6 +57,18 @@ class Pipeline(Thread):
         except Exception as e:
             raise HousingException(sys,e ) from e 
         
+    def strat_model_trainer( self, data_transformation_artifact : DataTransformationArtifact)->ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+                                        data_transformation_artifact=data_transformation_artifact )
+            
+            return model_trainer.initiate_model_trainer()
+        
+        except Exception as e:
+            raise HousingException(sys,e ) from e
+        
+
+    
 
     
         
